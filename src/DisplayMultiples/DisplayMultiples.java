e2b8673cbb0e03a36dd85de16e7fcf795b88457e
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-10-11 14:31:56 -0400

    changes (2)

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class DisplayMultiples {
 
     public static void main(String args[]) {
-       
+    	Scanner in = new Scanner (System.in);
+    	System.out.println("Give me a number!");
+    	int First= in.nextInt();
+    	for (int Numsec = 1; Numsec<=12; Numsec++) {
+    		System.out.println(Numsec*First);
+    	}
+    	
     }
 
 }
--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -9,7 +9,7 @@ public class ValidatePin {
         int pin =1234;
         System.out.println("Enter a PIN");
         int userPin=in.nextInt(); 
-        int userGuesses=1;
+//        int userGuesses=1;
         while (userPin !=pin) {
         	System.out.println("Invalid PIN. Try again."); 
         	userPin=in.nextInt();
